1b71a979d20236bacbd2fbe7fbf54e2c2bbaa0c1
ccma7 <ma.comunidadecrista@gmail.com>
2025-08-25 16:17:47 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 async function login() {
     const emailInput = document.getElementById('email').value.toLowerCase().trim();
+
+    if(!emailInput) {
+        document.getElementById('msg').innerText = "Digite seu email!";
+        return;
+    }
+
     const membros = await buscarMembros();
 
+    // Procurar pelo email digitado
     const membro = membros.find(m => m.email.toLowerCase().trim() === emailInput);
 
     if (membro) {
+        // Salvar dados no localStorage
         localStorage.setItem('membroLogado', JSON.stringify(membro));
+        // Redirecionar para a carteirinha
         window.location.href = "carteirinha.html";
     } else {
         document.getElementById('msg').innerText = "Email não encontrado!";
